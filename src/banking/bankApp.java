b4a0ed841e394a4cf867dfb296629b406473ae61
bastismo <jamesoliverbastismo188@gmail.com>
2025-08-08 17:39:44 +0800

    added new bankApp

---
+++ src/banking/bankApp.java
@@ -0,0 +1,53 @@
+package banking;
+
+public class bankApp {
+
+    private int accNo;
+    private double balance;
+    private int pin;
+
+    
+    public double getBalance() {
+        return balance;
+    }
+
+    public int getPin() {
+        return pin;
+    }
+
+    public int getAccNo() {
+        return accNo;
+    }
+
+    
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setPin(int pin) {
+        this.pin = pin;
+    }
+
+    public void setAccNo(int accNo) {
+        this.accNo = accNo;
+    }
+
+    
+    public boolean verifyAccount(int accn, int pn) {
+        return accNo == accn && pin == pn;
+    }
+
+   
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposit successful. New balance: ₱" + balance);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+   public void withdraw(double amount) {
+        
+    }
+}
